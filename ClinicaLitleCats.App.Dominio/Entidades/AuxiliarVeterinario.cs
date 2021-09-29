c5391904128dc41cd5dd2e819e7efa687aaba7d8
tecnycompu <tecnycompu@gmail.com>
2021-09-29 00:21:10 -0500

    CAMBIO EN ENTIDADES DESDE REPOSITORIO CLONADO

--- ClinicaLitleCats.App.Dominio/Entidades/AuxiliarVeterinario.cs
+++ ClinicaLitleCats.App.Dominio/Entidades/AuxiliarVeterinario.cs
@@ -2,9 +2,9 @@ using System;
 namespace ClinicaLitleCats.App.Dominio
 {
 
-    public class AuxiliarVeterinario
+    public class AuxiliarVeterinario : Persona
     {
-        public int IdAuxiliar {get;set;}
+        //public int IdAuxiliar {get;set;}
         public string TarjetaProfesional{get;set;}
         public int HorasLaborales{get;set;}
 
--- ClinicaLitleCats.App.Dominio/Entidades/Persona.cs
+++ ClinicaLitleCats.App.Dominio/Entidades/Persona.cs
@@ -4,7 +4,7 @@ namespace ClinicaLitleCats.App.Dominio
 
     public class Persona
     {
-        public int IdPersona {get;set;}
+        public int Id {get;set;}
         public string Nombre{get;set;}
         public string Apellido{get;set;}
         public string DocumentoIdentidad{get;set;}
