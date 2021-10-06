f3a8eafbd4712262ad35b23761bf9516f471e049
tecnycompu <tecnycompu@gmail.com>
2021-10-06 05:33:46 -0500

    GRABADO CORRECTO DE DB

--- ClinicaLitleCats.App.Consola/Program.cs
+++ ClinicaLitleCats.App.Consola/Program.cs
@@ -12,17 +12,31 @@ namespace ClinicaLitleCats.App.Consola
         static void Main(string[] args)
         {
             Console.WriteLine("Bienvenido al sistema!");
-            AddPacienteCat();
+            //AddPacienteCat();
+            BuscarPacienteCat(1);
 
         }
         private static void AddPacienteCat()
         {
             var pacienteCat = new PacienteCat{
                 Estado="Sano",
+                Alias="Minino",
+                Raza="Angora",
+                Edad="2 años",
+                Color="Negro",
+                               
 
             };
 
             _repoPacienteCat.AddPacienteCat(pacienteCat);
         }
+        private static void BuscarPacienteCat(int IdPacienteCat)
+        {
+
+            var pacienteCat=_repoPacienteCat.GetPacienteCat(IdPacienteCat);
+            Console.WriteLine(pacienteCat.Alias+" "+pacienteCat.Raza);
+
+        }
+
     }
 }
